70d7fe455babaf138e3af1b34f39512db11bde52
danishred <83757129+danishred@users.noreply.github.com>
2024-09-03 00:37:35 +0530

    Initialized Project

--- src/test/java/com/linvest21/employee_management_system/EmployeeManagementSystemApplicationTests.java
+++ src/test/java/com/linvest21/employee_management_system/EmployeeManagementSystemApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.linvest21.employee_management_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class EmployeeManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
